a2e066c20249f6c3851dc7bab5b2cd9f41687021
Christopher Nguyen <christopherx.nguyen@intel.com>
2018-07-25 15:36:00 -0700

    Added side navigation to webpage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-test-javascript = function () {
-  console.log('This is a test');
+// Navigation methods
+
+/*
+    - Remove open-nav-button
+    - Push main-container 15% to the left
+    - Display side navigation
+ */
+openNav = function () {
+    document.getElementById('open-nav-button').style.display = 'none';
+    document.getElementById('main-container').style.marginLeft = '15%';
+    document.getElementById('side-nav').style.display = 'inline-block';
+};
+
+/*
+    - Display open-nav-button
+    - Display main-container full screen
+    - Remove side-nav
+ */
+closeNav = function () {
+    document.getElementById('open-nav-button').style.display = 'inline';
+    document.getElementById('main-container').style.marginLeft = '0%';
+    document.getElementById('side-nav').style.display = 'none';
 };
